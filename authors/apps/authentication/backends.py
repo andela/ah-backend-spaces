1eddd4ece872bb43fd751526059b814822d47850
William Sserubiri <williamsserubiri@CEO-Pro.local>
2018-07-24 10:48:15 +0300

    [Chore

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -1,10 +1,14 @@
-# import jwt
-#
-# from django.conf import settings
-#
-# from rest_framework import authentication, exceptions
-#
-# from .models import User
+import jwt
+
+from django.conf import settings
+
+from rest_framework import authentication, exceptions
+
+from .models import User
 
 """Configure JWT Here"""
 
+class JWTAuthentication:
+
+    def __init__(self):
+        pass
