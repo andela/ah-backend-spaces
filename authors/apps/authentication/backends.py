125bec84d9fdaa4994e2b5bc61ae3e2f817afe67
John Joshua Abulo <johnjoshuaabulo@Abulo-JJs-Mac-Book-Pro.local>
2018-07-24 19:00:39 +0300

    [Chore #159082366] Upgrade Authors Haven Django app from Django 1.10 to Django 2.06

--- authors/apps/authentication/backends.py
+++ authors/apps/authentication/backends.py
@@ -1,10 +1,24 @@
-# import jwt
-#
-# from django.conf import settings
-#
-# from rest_framework import authentication, exceptions
-#
-# from .models import User
+import jwt
+
+from django.conf import settings
+
+from rest_framework import authentication, exceptions
+
+from .models import User
 
 """Configure JWT Here"""
 
+
+class JWTAuthentication:
+
+    def __init__(self):
+        """
+        Create a class instance here
+        """
+        pass
+
+    def authenticate(self, user_token):
+        """
+        Check Authenticity of a user token here
+        """
+        pass
