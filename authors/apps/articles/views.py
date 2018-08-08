7d8c7c0848a883d9f2dd7848290300fe432dddfb
abulo john joshua <john.abulo@andela.com>
2018-08-08 22:06:24 +0300

    [Feature #159082381] Add user rate article

--- authors/apps/articles/views.py
+++ authors/apps/articles/views.py
@@ -11,7 +11,7 @@ from ..authentication.models import User
 from .renderers import ArticlesJSONRenderer
 
 from .serializers import (
-    CreateArticleAPIViewSerializer
+    CreateArticleAPIViewSerializer, RatingArticleAPIViewSerializer
 )
 
 
@@ -43,3 +43,32 @@ class CreateArticleAPIView(RetrieveUpdateAPIView):
         serializer.save()
 
         return Response({"message": "Article created successfully."}, status=status.HTTP_201_CREATED)
+
+
+class RateArticleAPIView(RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated,)
+    renderer_classes = (ArticlesJSONRenderer,)
+    serializer_class = RatingArticleAPIViewSerializer
+
+    def post(self, request, article_id):
+        Rating = request.data.get('article', {})
+
+        # Add the article id to rating to be made
+        Rating["article_id"] = article_id
+
+        # decode user token and return its value
+        user_data = JWTAuthentication().authenticate(request)
+
+        # get id of the user rating an article
+        Rating["user_id"] = user_data[1]
+
+        # Notice here that we do not call `serializer.save()` like we did for
+        # the registration endpoint. This is because we don't actually have
+        # anything to save. Instead, the `validate` method on our serializer
+        # handles everything we need.
+
+        serializer = self.serializer_class(data=Rating)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({"message": "Article rated successfully."}, status=status.HTTP_201_CREATED)
